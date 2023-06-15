053cc789e82d7099e4f5962e055ecc2d2d21d23e
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 <dohyun911@hanmail.net>
2023-06-15 12:21:43 +0900

    design: members css 디자인 완성

---
+++ js/sub/members.js
@@ -0,0 +1,23 @@
+const membersSection = document.querySelector('.members');
+const memberBoxContainer = membersSection.querySelector('.memberBoxContainer');
+
+let tags = '';
+
+fetch('DB/members.json')
+	.then((res) => res.json())
+	.then((json) => {
+		console.log(json.item);
+		const membersData = json.members;
+
+		membersData.map((data) => {
+			tags += `
+       <div class="membersBox">
+       <div class="membersName"><h1>${data.name}</h1><h3>${data.sub}</h3></div>
+        <div class="membersImg" style="background-image : url(img/${data.pic})"></div>
+        <div class="membersDesc"><p>${data.description}</p></div>
+
+     </div>
+      `;
+		});
+		memberBoxContainer.innerHTML = tags;
+	});
