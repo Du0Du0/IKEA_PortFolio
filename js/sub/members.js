89942edf24e66d80f359cfd41890932deff95913
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 <dohyun911@hanmail.net>
2023-06-18 04:19:49 +0900

    feature: members 슬라이더 기능 추가

--- js/sub/members.js
+++ js/sub/members.js
@@ -21,3 +21,53 @@ fetch('DB/members.json')
 		});
 		memberBoxContainer.innerHTML = tags;
 	});
+
+//슬라이더
+
+// Variables
+
+// Set index to 0
+let currentSlide = 0;
+// Store all slides
+const slides = document.querySelectorAll('.slider .slide');
+// Store total number of slides
+const totalSlides = slides.length;
+
+// Functions
+// ----------------------------
+
+// Hides all slides, Displays Current index
+function cycleSlides() {
+	// stores current slide
+	const slide = slides[currentSlide];
+	// hides all slides
+	slides.forEach((slide) => {
+		slide.style.display = 'none';
+	});
+	// display current slide
+	slide.style.display = 'inline-block';
+}
+
+// call click function when next button is clicked
+document.querySelector('.next').addEventListener('click', function () {
+	// update current slide index
+	currentSlide += 1;
+	// if on last slide, reset index to 0
+	if (currentSlide > totalSlides - 1) {
+		currentSlide = 0;
+	}
+	// run cycle slides function to display slide
+	cycleSlides();
+});
+
+// call click function when prev button is clicked
+document.querySelector('.prev').addEventListener('click', function () {
+	// update current slide index
+	currentSlide -= 1;
+	// if on first slide, move to last slide
+	if (currentSlide < 0) {
+		currentSlide = totalSlides - 1;
+	}
+	// run cycle slides function to display slide
+	cycleSlides();
+});
