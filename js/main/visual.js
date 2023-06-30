18d474e1edc7f28e4b7f62fd05f4348dfe4853e1
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 <dohyun911@hanmail.net>
2023-06-30 20:26:51 +0900

    feature: visual 배경바뀌는 스크립트 수정

--- js/main/visual.js
+++ js/main/visual.js
@@ -10,7 +10,10 @@ pointBox1.addEventListener('click', (e) => {
 	pointBox2.classList.remove('on');
 
 	if (e.target === pointBox1) {
-		visualSection.style.background = 'url(img/news10.png)';
+		visualSection.style.background = 'url(img/visual1.png)';
+		for (const bg of bgs) bg.style.borderColor = 'rgba(59, 59, 59, 0.8)';
+		pointBox1.style.borderColor = '#eee';
+		pointBox2.style.borderColor = '#eee';
 		visualSection.style.backgroundPosition = 'center';
 		visualSection.style.backgroundRepeat = 'no-repeat';
 		visualSection.style.backgroundSize = 'cover';
@@ -25,7 +28,10 @@ pointBox2.addEventListener('click', (e) => {
 	pointBox1.classList.remove('on');
 
 	if (e.target === pointBox2) {
-		visualSection.style.background = 'url(img/visual1.png)';
+		visualSection.style.background = 'url(img/news10.png)';
+		for (const bg of bgs) bg.style.borderColor = 'rgba(159, 159, 159, 0.5)';
+		pointBox1.style.borderColor = '#eee';
+		pointBox2.style.borderColor = '#eee';
 		visualSection.style.backgroundPosition = 'center';
 		visualSection.style.backgroundRepeat = 'no-repeat';
 		visualSection.style.backgroundSize = 'cover';
